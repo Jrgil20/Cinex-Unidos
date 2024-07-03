db9a4571ed49f5792ec1bb558ec134d146531492
jrgil.20 <jrgil.20@est.ucab.edu.ve>
2024-07-03 20:34:21 +0000

    no se wey, un commit

---
+++ Mockup/js/cines.js
@@ -0,0 +1,28 @@
+fetch('https://cinexunidos-production.up.railway.app/theatres', {
+  method: 'GET',
+
+}).then(response => response.json())
+   .then(data => {
+    console.log(data);
+    const cinesContainer = document.getElementById('cines-container');
+    data.forEach(cine => {
+        const carta = document.createElement('div');
+        carta.classList.add('card');
+        const nombreCine = document.createElement('h3');
+        nombreCine.textContent = cine.name;
+        const direccion = document.createElement('p');
+        direccion.textContent = cine.location;
+
+        carta.addEventListener('click', () => {
+            window.location.href = 'cine.html?cine='+ cine.id;
+        });
+
+        carta.appendChild(nombreCine);
+        carta.appendChild(direccion);
+        cinesContainer.appendChild(carta);
+    });
+  })
+.catch(error => {
+   console.error('Error:', error);
+});
+
