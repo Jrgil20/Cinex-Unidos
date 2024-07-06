cdae95b53d99075115a6e3d186107c8a2ac4c726
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-07-06 17:11:27 -0400

    refactor: Update CSS styles for seating and card components
    The code changes in the funcion.css and sala.css files update the CSS styles for the seating and card components. The changes include modifying the background color, border, and size of the seating elements, as well as adjusting the background color and border radius of the card elements. These updates improve the visual appearance and consistency of the website.

---
+++ Mockup/js/funcion.js
@@ -0,0 +1,14 @@
+const id = new URLSearchParams(window.location.search).get('cine');
+const idAuditorio = new URLSearchParams(window.location.search).get('auditorio');
+const idFuncion = new URLSearchParams(window.location.search).get('showtime');
+
+fetch(`https://cinexunidos-production.up.railway.app/theatres/${id}/auditoriums/${idAuditorio}/showtimes/${idFuncion}`, {
+    method: 'GET',
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+.catch(error => {
+    console.error('Error:', error);
+});
--- Mockup/js/sala.js
+++ Mockup/js/sala.js
@@ -13,7 +13,37 @@ fetch(`https://cinexunidos-production.up.railway.app/theatres/${id}/auditoriums/
     informacionContainer.appendChild(nombreAuditorio);
     informacionContainer.appendChild(capacidadAuditorio);
 
+    const showtimes = data.showtimes;
+    const funcionescontainer = document.getElementById('funciones-container');
+    const funcionTitle = document.createElement('h2');
+    funcionTitle.textContent = 'Funciones';
+    showtimes.forEach(showtime => {
+      const card = document.createElement('div');
+      card.classList.add('card');
+      card.classList.add('funcion-card');
 
+      const poster = document.createElement('img');
+      poster.src = showtime.movie.poster;
+
+      const pelicula = document.createElement('h3');
+      pelicula.textContent = `Pelicula: ${showtime.movie.name}`;
+
+      const rating = document.createElement('p');
+      rating.textContent = `Rating: ${showtime.movie.rating}`;
+
+      const hora = document.createElement('h4');
+      hora.textContent = `Hora: ${showtime.startTime}`;
+
+
+      card.appendChild(poster);
+      card.appendChild(pelicula);
+      card.appendChild(rating);
+      card.appendChild(hora);
+      card.addEventListener('click', () => {
+        window.location.href = `funcion.html?cine=${id}&auditorio=${idAuditorio}&showtime=${showtime.id}`;
+      });
+      informacionContainer.appendChild(card);
+    });
 
   })
   .catch(error => {
