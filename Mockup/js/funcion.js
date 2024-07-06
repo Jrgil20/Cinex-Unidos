c7b9e5b1b1b106999d5bed8b5389d638a4282dd4
JRGIL12 <108557695+JRGIL12@users.noreply.github.com>
2024-07-06 17:34:19 -0400

    refactor: Update CSS styles for seating and card components

--- Mockup/js/funcion.js
+++ Mockup/js/funcion.js
@@ -8,6 +8,31 @@ fetch(`https://cinexunidos-production.up.railway.app/theatres/${id}/auditoriums/
 .then(response => response.json())
 .then(data => {
     console.log(data);
+
+    const informacionfuncion = document.getElementById('informacion-container');
+    const pelicula = document.createElement('h2');
+    pelicula.textContent = data.movie.name;
+    informacionfuncion.appendChild(pelicula);
+
+    const poster = document.createElement('img');
+    poster.src = data.movie.poster;
+    informacionfuncion.appendChild(poster);
+
+    const horario = document.createElement('p');
+    horario.textContent = data.startTime;
+    informacionfuncion.appendChild(horario);
+
+    const rating = document.createElement('p');
+    rating.textContent = data.movie.rating;
+    informacionfuncion.appendChild(rating);
+
+    const duracion = document.createElement('p');
+    duracion.textContent = data.movie.runtime;
+    informacionfuncion.appendChild(duracion);
+
+
+    
+
 })
 .catch(error => {
     console.error('Error:', error);
